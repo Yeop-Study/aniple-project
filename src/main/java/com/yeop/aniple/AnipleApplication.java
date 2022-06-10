fc81faf98e669eb65562e5e50881eed3664a87c6
FitpetEdgar <92002898+FitpetEdgar@users.noreply.github.com>
2022-06-10 11:45:19 +0900

    init commit

---
+++ src/main/java/com/yeop/aniple/AnipleApplication.java
@@ -0,0 +1,13 @@
+package com.yeop.aniple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnipleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeop/aniple/AnipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeop.aniple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnipleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
